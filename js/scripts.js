fd5a833d6b4f2fb253677761acb21cab72855e8b
Big Gregory <gregory11031985@gmail.com>
2018-03-07 10:06:04 +0200

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+
+  new WOW().init();
+
+  $('.collapse').on('show.bs.collapse', function(){
+    $(this).prev().find('.fa').removeClass().addClass('fa fa-minus-circle');
+  });
+
+  $('.collapse').on('hide.bs.collapse', function(){
+    $(this).prev().find('.fa').removeClass().addClass('fa fa-plus-circle');
+  });
+
+  $('#myForm').validator();
+
+  $('.form-sel').select2({
+    minimumResultsForSearch: Infinity
+  });
+
+  // Smooth scroll to main menu
+  $('.scroll').click(function(event){
+    event.preventDefault();
+
+    var id = $(this).attr('href');
+    var top = $(id).offset().top;
+
+    $('body,html').animate({scrollTop: top}, 1000);
+
+  });
+  // Smooth scroll to register form
+  // $('#register').click(function(event){
+  //   event.preventDefault();
+  //
+  //   var id = $(this).attr('href');
+  //   var top = $(id).offset().top;
+  //
+  //   $('body,html').animate({scrollTop: top}, 1200);
+  //
+  // });
+
+});
