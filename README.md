b50a40e6f48f982d38b652346e60825bc996fe19
Big Gregory <gregory11031985@gmail.com>
2018-03-09 15:39:55 +0200

    google maps API added

--- README.md
+++ README.md
@@ -4,7 +4,8 @@
 Example of landing page you are free to use in your projects
 
 # Technologies: 
-Bootstrap, WOW.js with Animate.css, form validation Bootatrap plugin, select2 Bootstrap plugin
+Bootstrap, WOW.js with Animate.css, form validation Bootatrap plugin, select2 Bootstrap plugin,
+Google fonts, Google Maps Api, Font Awesome
 
 #  https://biggregory.github.io/Cyber-Forum_landing_page/
 
--- js/scripts.js
+++ js/scripts.js
@@ -19,6 +19,7 @@ $(document).ready(function(){
   });
 
   // Smooth scroll to main menu
+  // and register form
   $('.scroll').click(function(event){
     event.preventDefault();
 
@@ -28,15 +29,31 @@ $(document).ready(function(){
     $('body,html').animate({scrollTop: top}, 1000);
 
   });
-  // Smooth scroll to register form
-  // $('#register').click(function(event){
-  //   event.preventDefault();
-  //
-  //   var id = $(this).attr('href');
-  //   var top = $(id).offset().top;
-  //
-  //   $('body,html').animate({scrollTop: top}, 1200);
-  //
-  // });
+
 
 });
+function initMap(){
+  // Map options
+  var sydney = {lat: -33.865143, lng: 151.209900};
+  var options = {
+    zoom: 8,
+    center: sydney
+  }
+
+  // New map
+  var map = new google.maps.Map(document.getElementById('map'), options);
+
+  // Add Marker
+  var marker = new google.maps.Marker({
+    position: sydney,
+    map: map,
+  });
+
+  //Info window
+  var infoWindow = new google.maps.InfoWindow({
+    content: '<h3>Cyber-Security Forum</h3>'
+  });
+  marker.addListener('click', function(){
+    infoWindow.open(map, marker)
+  });
+}
